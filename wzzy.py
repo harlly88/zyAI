0e81d8dabcb23c80a95eb7439e14fcf3e66146f5
harlly88 <56230482+harlly88@users.noreply.github.com>
2020-03-08 19:51:05 +0800

    程序增加剂量
    1、程序增加剂量
    2、增加了抗新冠病毒名中药方清肺排毒汤，并加入了仝小林院士的增减辩证。

--- wzzy.py
+++ wzzy.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 qzyz = 0.2    #权重因子0-1，就是在一串症状中，有多少比例的症状是有效的
 
-st.title("中医AI问诊V1.1")
+st.title("中医AI问诊V2.0")
 
 import os
 path_list = os.listdir()
@@ -38,7 +38,7 @@ zhenzhuangXZ_set=set(zhenzhuangXZ)
 
 #开始症状分析
 zhenzhuangGL=set()
-yaofang=set()
+yaofang=list()
 zhenduan=set()
 quanzhong=[0,0,0] #症状对应每个方剂的权重
 for i in range(3,Sheet1.max_row+1):
@@ -66,6 +66,16 @@ for _ in range(3):
     max_index.append(index)
 t = []
 
+def is_number(s):  #判断是否数字的函数
+	s1=['0','1','2','3','4','5','6','7','8','9','+','-'];
+	try:
+		if s in s1:
+			return True
+	except ValueError:
+		pass
+	return False
+	
+yaofang_name=list()
 if max_number[2] < qzyz :  #移除权重过低的答案
 	max_number.pop()
 	max_index.pop()
@@ -78,19 +88,57 @@ else:
 	for i in range(len(max_index)) :
 		zhenduan.add(Sheet1.cell(max_index[i],11).value)   #最接近症状的诊断
 		zhenduan.add(Sheet1.cell(max_index[i],12).value)
-		for j in range(16,30):
-			yaofang.add(Sheet1.cell(max_index[i],j).value)
+		for j in range(16,Sheet1.max_column+1):
+			yaofang.append(Sheet1.cell(max_index[i],j).value)
+	
+	yaofang_hanjilian=False   #判断药方里是否含有剂量
+	k1=''			
+	for k in yaofang:  #k1为药方里面的中药名
+		if k:
+			k1=k
+			if is_number(k[-1]):
+				yaofang_hanjilian=True
+				k1 = k[:-1]
+				if is_number(k[-2]):
+					k1 = k[:-2]
+					if is_number(k[-3]):
+						k1 = k[:-3]
+						if is_number(k[-4]):
+							k1 = k[:-4]
+			if not k1 in yaofang_name:
+				yaofang_name.append(k1)  #yaofang_name为纯粹中药名的表
+	
+	if yaofang_hanjilian:
+		yaofang_list=list()
+		for l in yaofang_name:   #计算每样药材的量
+			l1=0
+			for m in yaofang:
+				if m:
+					l3=''
+					if l in m:
+						l2=m[len(l):]
+						if not l2=='':
+							if l2[0]=='+' or l2[0]=='-':
+								if l2[0]=='+':
+									l1=l1+int(l2[1:])
+								if l2[0]=='-':
+									l1=l1-int(l2[1:])
+							else:
+								l1=l1+int(l2)
+			if l1>0:
+				l3=l + str(l1)
+				yaofang_list.append(l3)
+	else:
+		yaofang_list=yaofang_name
+	
 	zhenduan.discard(None)
-	yaofang.discard(None)
 	zhenduan_list=list(zhenduan)
 	zhenduan_list.sort()
-	yaofang_list=list(yaofang)
-	yaofang_list.sort()
-	str = " "
+	str = "，"
 	st.write('诊断及建议: ')
 	st.write(str.join(zhenduan_list))
 	st.write('建议中药：',str.join(yaofang_list))
-	st.write('中药剂量及服用有严格的要求，请在合格中医师指导下进行，我们不对服用中药后引起的任何后果负责')
+	st.write('*中药剂量及服用有严格的要求，请在合格中医师指导下进行，我们不对服用中药后引起的任何后果负责')
 
 
 #© 2020 GitHub, Inc.
